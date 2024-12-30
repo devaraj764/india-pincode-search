00944e82a63dd83aae1248be84d37b38751aee62
devaraj764 <deva170725@gmail.com>
2024-12-30 15:52:22 +0530

    Changes

--- api/pincodes.js
+++ api/pincodes.js
@@ -1,9 +1,11 @@
+// api/pincodes.js
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("./db.json");
+const router = jsonServer.router("db.json"); // The path to your db.json file
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-
-module.exports = server;
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
